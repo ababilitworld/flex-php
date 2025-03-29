6d01552c537d30a31e8b9f1ad1f51447ae8ec1a5
Ababil IT World <ababilitworld@gmail.com>
2025-03-30 03:20:48 +0600

    ababilitworld/2025-03-30-03-20

--- src/Package/Mixin/Standard/V1/V1.php
+++ src/Package/Mixin/Standard/V1/V1.php
@@ -2,8 +2,10 @@
 
 namespace Ababilithub\FlexPhp\Package\Mixin\Standard\V1;
 
-use AbabilIthub\FlexPhp\Package\Mixin\Singleton\Instance\Single\V1\V1 as Singleton;
-use Ababilithub\FlexPhp\Package\Mixin\Data\Access\Static\V1\V1 as AccessStaticMember;
+use AbabilIthub\{
+    FlexPhp\Package\Mixin\Singleton\Instance\Single\V1\V1 as Singleton,
+    FlexPhp\Package\Mixin\Data\Access\Static\V1\V1 as AccessStaticMember
+};
 
 trait V1 
 {
