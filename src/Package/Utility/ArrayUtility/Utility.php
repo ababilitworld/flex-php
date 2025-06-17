078862c50742b7c23729d704d805ed8b944fb67e
Ababil IT World <ababilitworld@gmail.com>
2025-06-17 06:38:30 +0600

    ababilithub/2025-06-17-06-38

---
+++ src/Package/Utility/ArrayUtility/Utility.php
@@ -0,0 +1,30 @@
+<?php
+namespace Ababilithub\FlexPhp\Package\Utility\ArrayUtility;
+
+use Ababilithub\{
+    FlexPhp\Package\Mixin\V1\Standard\Mixin as StandardMixin,
+};
+class Utility
+{
+    use StandardMixin;
+
+    public static function search_array(array $needleArray, array $haystack, $keys = 'id'): array|bool    
+    {
+        // Single key search
+        if (is_string($keys)) 
+        {
+            return in_array($needleArray[$keys], array_column($haystack, $keys));
+        }
+        
+        // Multiple key search
+        return array_filter($haystack, function($item) use ($needleArray, $keys) {
+            foreach ($keys as $key) {
+                if (!isset($item[$key]) || $item[$key] !== $needleArray[$key]) {
+                    return false;
+                }
+            }
+            return true;
+        });
+
+    }
+}
